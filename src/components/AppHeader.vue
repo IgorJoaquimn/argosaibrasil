591434e5b35e87291565d1cbed043a8f6d9b06b4
Marisa Vasconcelos <marisavas@yahoo.com>
2025-07-17 21:11:43 -0300

    put mobile consent form

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,14 +3,17 @@
     <div class="w-full max-w-7xl mx-auto px-6 md:px-12 lg:px-24">
       <div class="flex items-center justify-between w-full py-4">
         <!-- Logo + Title Section (Left) -->
-        <div class="flex items-center flex-shrink-0 gap-2">
+        <div class="flex items-center gap-2">
+         
+          <img alt="Argos IA Brasil" src="@/assets/logo_argos.png" class="w-[100px] h-[100px] sm:w-[80px] sm:h-[80px] md:w-[100px] md:h-[100px]" />
+        <h1 class="flex-1 text-xl sm:text-3xl lg:text-4xl font-semibold text-primary-500 break-words">
+ 
           
-          <img alt="Argos IA Brasil" src="@/assets/logo_argos.png" class="w-[100px] h-[100px]" />
-
-          <h1 class="text-2xl sm:text-3xl lg:text-4xl font-semibold text-primary-500">
             Argos IA Brasil
           </h1>
         </div>
+
+
 
       
 
